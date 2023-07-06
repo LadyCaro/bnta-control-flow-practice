81b5309ee0ca464d61f31a6d658d19095c9a6fa0
LadyCaro <128640816+LadyCaro@users.noreply.github.com>
2023-07-06 16:48:10 +0200

    Update weatherCheck conditions

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,15 +1,19 @@
 public class WeatherCheck {
     public static void main(String[] args) {
 
-        int temperature = 19;
-        boolean currentlyRaining = true;
+        int temperature = 10;
+        boolean currentlyRaining = false;
 
-    if(!currentlyRaining && temperature >= 20){
-        System.out.println("Wear sunscreen and t-shirt");
-    } else if(!currentlyRaining && temperature < 20){
-        System.out.println("Bring a jumper");
-    } else {
-        System.out.println("Bring an umbrella");
+        if (!currentlyRaining && temperature >= 50) {
+            System.out.println("Prepare to be human ice-cream (melted version ofc)");
+        } else if (!currentlyRaining && temperature < 20) {
+            System.out.println("Bring a jumper");
+        } else if(currentlyRaining && temperature < 20) {
+            System.out.println("Bring a jumper and umbrella");
+        } else if (!currentlyRaining && temperature >= 20) {
+            System.out.println("Wear sunscreen and t-shirt");
+        } else {
+        System.out.println("Bring an umbrella and wear a t-shirt");
     }
     }
 }
